3b7588d9a7b5915a7a16209b7194aa932fd48c30
turtle <769711153@qq.com>
2022-12-19 00:59:19 +0800

    修改启动参数

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import uvicorn
 
 # 入口
 if __name__ == '__main__':
-    uvicorn.run("api.api:app", port=8000, reload=True, access_log=True)
+    uvicorn.run("api.api:app",host='0.0.0.0', port=7000, reload=True, access_log=True)
